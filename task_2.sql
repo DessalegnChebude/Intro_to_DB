ee33d118fa5e5b40cac394b9b2520966f5a71508
DessalegnChebude <letgo900@gmail.com>
2024-10-20 20:36:57 +0300

    table creations

---
+++ task_2.sql
@@ -0,0 +1,36 @@
+USE alx_book_store
+
+-- creating authors table
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+)
+-- creating books table
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+)
+-- Creating customers table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+-- creating Orders table
+CREATE TABLE Orders  (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
+
+-- creating Order Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+)
