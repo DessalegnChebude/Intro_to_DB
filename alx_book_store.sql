356fd074678031c5d16b4fe76be76bc6d56ec891
DessalegnChebude <letgo900@gmail.com>
2024-10-20 19:38:05 +0300

    fixing some bugs on the table creation and give descriptions

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,13 @@
+-- creating and using alx_book_store database
 CREATE DATABASE IF NOT EXISTS alx_book_store
 USE alx_book_store
 
+-- creating authors table
 CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 )
+-- creating books table
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
@@ -12,21 +15,24 @@ CREATE TABLE Books (
     price DOUBLE,
     publication_date DATE
 )
+-- Creating customers table
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 )
+-- creating Orders table
 CREATE TABLE Orders  (
     order_id INT PRIMARY KEY,
-    customer_id FOREIGN KEY referencing Customers TABLE,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 )
 
+-- creating Order Details table
 CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
-    order_id FOREIGN KEY referencing Orders TABLE,
-    book_id FOREIGN KEY referencing Books TABLE,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
