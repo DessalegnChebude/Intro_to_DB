ba094799e43cf5783710baec49c5755ef8469bf3
DessalegnChebude <letgo900@gmail.com>
2024-10-20 21:06:17 +0300

    describe table info with out describe or explain key words

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,7 @@
-USE alx_book_store
-SELECT * FROM books
+-- Script to display the full description of the 'Books' table
+SELECT 
+    COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
