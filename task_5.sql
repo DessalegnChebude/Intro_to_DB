50680eb7e507957876e6f24b090018a805072b8c
DessalegnChebude <letgo900@gmail.com>
2024-10-20 21:22:49 +0300

    add multiple customer info at once

--- task_5.sql
+++ task_5.sql
@@ -3,4 +3,6 @@ INSERT INTO customer (
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT)
-    VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
+    VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+            (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+            (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
